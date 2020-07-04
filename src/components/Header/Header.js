bb48e63b530c52ac3c7442f839c8cdbc8a139f17
wanho1108 <wanho1108@gmail.com>
2020-07-05 01:48:30 +0900

    #3.1 CSS in React part Two

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Header.css";
+import styles from "./Header.module.scss";
 
 export default () => (
-  <header className="nav">
-    <ul>
+  <header>
+    <ul className={styles.navList}>
       <li>
         <a href="/">Movies</a>
       </li>
