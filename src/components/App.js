950f4e0ab5a19ffef700a79b9eb82101fa36e7d2
wanho1108 <wanho1108@gmail.com>
2020-07-05 01:29:18 +0900

    #2.2 React Router part Two

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 import Router from "components/Router";
+import Header from "components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Router />
     </>
   );
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "routes/Home";
 import TV from "routes/TV";
 import Search from "routes/Search";
 
 export default () => (
   <Router>
-    <Route path="/" exact component={Home} />
-    <Route path="/tv" component={TV} />
-    <Route path="/search" component={Search} />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" component={TV} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
