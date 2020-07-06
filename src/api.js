0160d1eec7ba92ae45f517e4be8437388540e05d
wanho1108 <wanho1108@gmail.com>
2020-07-06 15:40:50 +0900

    #5.3 TV Container

--- src/api.js
+++ src/api.js
@@ -29,7 +29,7 @@ export const moviesApi = {
 export const tvApi = {
   topRated: () => api.get("tv/top_rated"),
   popular: () => api.get("tv/popular"),
-  airingToday: () => api.get("tv/airingToday"),
+  airingToday: () => api.get("tv/airing_today"),
   showDetail: (id) =>
     api.get(`tv/${id}`, {
       params: {
