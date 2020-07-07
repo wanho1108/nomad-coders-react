2e537a4d8a4937f131918180e51766c5070e2f92
wanho1108 <wanho1108@gmail.com>
2020-07-08 00:18:10 +0900

    #6.2 TVPresenter and Loader Components

---
+++ src/components/Loader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: 100vw;
+  margin-top: 20px;
+  font-size: 32px;
+`;
+
+export default () => (
+  <Container>
+    <span role="img" aria-label="Loading">
+      ⏳
+    </span>
+  </Container>
+);
--- src/routes/TV/TVPresenter.js
+++ src/routes/TV/TVPresenter.js
@@ -1,9 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "components/Section";
+import Loader from "components/Loader";
+
+const Container = styled.div`
+  padding: 0 10px;
+`;
 
 const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
-  null;
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today Shows">
+          {airingToday.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
