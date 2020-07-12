8bf19a7c31904b388c0b899181dec3e6e0660317
wanho1108 <wanho1108@gmail.com>
2020-07-12 21:54:58 +0900

    #6.7 Poster Component part Two

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  width: 100vw;
+  width: 100%;
   height: 100vw;
   margin-top: 20px;
   font-size: 32px;
--- src/components/Poster.js
+++ src/components/Poster.js
@@ -3,23 +3,63 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+  height: 180px;
+  border-radius: 4px;
+  background-image: url(${(props) => props.bgUrl});
+  background-size: cover;
+  background-position: 50% 50%;
+  transition: opacity 0.1s linear;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
+  opacity: 0;
+  transition: opacity 0.1s linear;
+`;
 
-const Rating = styled.span``;
+const ImageContainer = styled.div`
+  position: relative;
+  margin-bottom: 5px;
 
-const Title = styled.span``;
+  &:hover {
+    ${Image} {
+      opacity: 0.8;
+    }
 
-const Year = styled.span``;
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
+
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
+
+const Year = styled.span`
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
   <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
     <Container>
       <ImageContainer>
-        <Image bgUrl={imageUrl} />
+        <Image
+          bgUrl={
+            imageUrl
+              ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+              : require("../assets/noPosterSmall.png")
+          }
+        />
         <Rating>
           <span role="img" aria-label="rating">
             ⭐
@@ -27,7 +67,9 @@ const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
           {rating}/10
         </Rating>
       </ImageContainer>
-      <Title>{title}</Title>
+      <Title>
+        {title.length > 18 ? `${title.substring(0, 15)}...` : title}
+      </Title>
       <Year>{year}</Year>
     </Container>
   </Link>
