8eb2747c40c24edf42c19cf2177dfb36b44069de
wanho1108 <wanho1108@gmail.com>
2020-07-13 11:23:21 +0900

    #8.7 Styled Components and Typescript

--- typescript/src/Number.tsx
+++ typescript/src/Number.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.span``;
+const Container = styled.span<{ isRed: boolean }>`
+  color: ${(props) => (props.isRed ? "red" : "black")};
+`;
 
 interface IProps {
   count: number;
 }
 
-const Number: React.FC<IProps> = ({ count }) => <Container>{count}</Container>;
+const Number: React.FunctionComponent<IProps> = ({ count }) => (
+  <Container isRed={count > 10}>{count}</Container>
+);
 
 export default Number;
