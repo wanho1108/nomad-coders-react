3ce05bd6f35242b69b2eefd8b0d009aec87f4e1d
wanho1108 <wanho1108@gmail.com>
2020-07-16 01:24:01 +0900

    #9.6 Understanding useReducer

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
-import Lang from "./context";
-import Screen from "./Screen";
-import translations from "./translations";
+import React, { useReducer } from "react";
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
   return (
-    <Lang defaultLang="en" translations={translations}>
-      <Screen />
-    </Lang>
+    <>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>Increment</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>Decrement</button>
+    </>
   );
 }
 
